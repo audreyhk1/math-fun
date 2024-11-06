f0894f5e0e7f9059595b04626068e357a05016a2
audreyhk1 <120613322+audreyhk1@users.noreply.github.com>
2024-11-06 03:30:19 +0000

    getting rid of some bugs

--- prime-colors.py
+++ prime-colors.py
@@ -4,33 +4,57 @@ import numpy as np
 # from colorutils import Color
 from PIL import Image
 import matplotlib.pyplot as plt
+from colorutils import Color
 global N_PRIMES
 N_PRIMES = 310248241 
 
 def main():
     global N_PRIMES
-    primes = get_primes(N_PRIMES)
+    primes_numbers, primes_colors = get_primes(N_PRIMES)
     
     # colors = np.array([0, 0, 0])
     # for i in range(1, 200):
     #     colors = np.concatenate((colors, [i, i, i]), axis=0)
     # color_image = Image.fromarray(colors, mode="RGB")
     
-    color_image.save("primes.jpeg")
+    # color_image.save("primes.jpeg")
     
+    pixel_arr = []
+    
+    prime_index = 0
     # # loop through every number
-    for i in range(1, 100 + 1):
+    for i in range(2, 7):
         # is i prime?
-        if i in primes:
-            mix_colors()
+        if i in primes_numbers:
+            pixel_arr.append(primes_colors[prime_index])
+            prime_index += 1
         # is i composite?
         else:
+            pixel = []
             factors = get_prime_factors(i)
+            # loop through factors
+            for factor in factors[1:]:
+                # get factor color from index in primes_number
+                try:
+                    color = primes_colors[primes_numbers.index(factor)]
+                    if factor != factors[1]:
+                        pixel = mix_colors(pixel, color)
+                    else:
+                        pixel = color
+                except ValueError:
+                    pass
+                print(pixel)
+        
+    print(pixel_arr)
+    
+                
+                
             
 # get primes
 # courtesy of https://stackoverflow.com/questions/11619942/print-series-of-prime-numbers-in-python
 def get_primes(end_prime):
-    primes = [2]
+    primes_colors = []
+    primes_numbers = [2]
 
     r, g, b = 0, 1, 1
     r_flag, g_flag, b_flag = True, False, False
@@ -38,28 +62,31 @@ def get_primes(end_prime):
         if all(num % i != 0 for i in range(2,int(math.sqrt(num))+1)):
             # add to an array
             if r_flag:
-                primes.append({"n": num, "color": [r, 0, 0]})
+                primes_colors.append([r, 0, 0])
+                primes_numbers.append(num)
                 r += 1
                 if r > 256:
                     g_flag = True
                     r_flag = False
             elif g_flag:
-                primes.append({"n": num, "color": [0, g, 0]})
+                primes_colors.append([0, g, 0])
+                primes_numbers.append(num)
                 g += 1
                 if g > 256:
                     b_flag = True
                     g_flag = False
             elif b_flag and b < 256:
-                primes.append({"n": num, "color": [0, 0, b]})
+                primes_colors.append([0, 0, b])
+                primes_numbers.append(num)
                 b += 1
                 
             else:
-                return primes
+                return primes_numbers, primes_colors
 
-# mix colors - https://github.com/edaniszewski/colorutils?tab=readme-ov-file#33-color-arithmetic
+# mix colors - https://github.com/scrtwpns/mixbox/tree/master/python
 # make primes brighter, composites lighter
-def mix_colors(factors, primes_list):
-    pass
+def mix_colors(color1, color2):
+    Color(tuple(color1), arithmetic=ArithmeticModel.BLEND) + Color(tuple(color2))
 
 
 # get primes of composite number 
